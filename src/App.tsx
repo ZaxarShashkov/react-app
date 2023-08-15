6694ee914941525a04789f071feb09806157ae39
ZaxarShashkov <zekharjah@gmail.com>
2023-08-15 21:55:44 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useInsertionEffect } from 'react';
 import './App.scss';
+import { fetchUsers, userSlice } from './store/reducers/UserSlice';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
 
 function App() {
-	return <div className='App'>hello</div>;
+	const dispatch = useAppDispatch();
+	const { count, isLoading, error, users } = useAppSelector((state) => state.userReducer);
+	const { increment } = userSlice.actions;
+
+	useEffect(() => {
+		fetchUsers(dispatch);
+	}, []);
+
+	console.log(users);
+	return (
+		<div className='App'>
+			<h1>{count}</h1>
+			<button onClick={() => dispatch(increment(1))}>Increment</button>
+			{!isLoading
+				? users.map((user) => {
+						return (
+							<>
+								<h1>{user.id}</h1>
+								<h2>{user.name}</h2>
+							</>
+						);
+				  })
+				: null}
+		</div>
+	);
 }
 
 export default App;
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,25 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IUser } from '../../interfaces/IUser';
+import axios from 'axios';
+import { AppDispatch } from '../store';
 
 interface UserState {
 	users: IUser[];
 	isLoading: boolean;
 	error: string;
+	count: number;
 }
 
 const initialState: UserState = {
 	users: [],
 	isLoading: false,
 	error: '',
+	count: 0,
+};
+
+export const fetchUsers = async (dispatch: AppDispatch) => {
+	try {
+		dispatch(userSlice.actions.usersFetching());
+		const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+		dispatch(userSlice.actions.usersFetchingSuccess(response.data));
+	} catch (e: any) {
+		dispatch(userSlice.actions.usersFetchingError(e.message));
+	}
 };
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState: initialState,
-    reducers: {
-
-    }
-})
-
+	name: 'user',
+	initialState: initialState,
+	reducers: {
+		increment(state, action: PayloadAction<number>) {
+			state.count += action.payload;
+		},
+		usersFetching(state) {
+			state.isLoading = true;
+		},
+		usersFetchingSuccess(state, action: PayloadAction<IUser[]>) {
+			state.isLoading = false;
+			state.error = '';
+			state.users = action.payload;
+		},
+		usersFetchingError(state, action: PayloadAction<string>) {
+			state.isLoading = false;
+			state.error = action.payload;
+		},
+	},
+});
 
 export default userSlice.reducer;
