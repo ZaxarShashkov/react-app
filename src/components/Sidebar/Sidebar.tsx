ea84f0d880deaae2e0c17efa9f56ed59584a39ef
ZaxarShashkov <zekharjah@gmail.com>
2023-08-26 13:38:26 +0300

    add router

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,18 +7,21 @@ const Sidebar = () => {
 		<div className={styles.sidebar}>
 			<nav className={styles.main__nav}>
 				<ul className={styles.main__menu_list}>
-					<li className={styles.main__menu_item}>Home Page</li>
 					<li className={styles.main__menu_item}>
-						<NavLink end to='/'>
+						<NavLink end to='/' className={styles.main__menu_item}>
+							Home Page
+						</NavLink>
+					</li>
+					<li className={styles.main__menu_item}>
+						<NavLink end to='/' className={styles.main__menu_item}>
 							Posts
 						</NavLink>
 					</li>
-					<li className={styles.menu__item}>
-						<NavLink end to='/Users' className={styles.menu__item}>
+					<li className={styles.main__menu_item}>
+						<NavLink end to='/Users' className={styles.main__menu_item}>
 							Users
 						</NavLink>
 					</li>
-					<li className={styles.main__menu_item}>Etc</li>
 				</ul>
 			</nav>
 		</div>
