f3a277aa6718541d32fcae8ac74b2cce242e45bf
ZaxarShashkov <zekharjah@gmail.com>
2023-09-03 13:47:27 +0300

    add comments for post

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
-import { IPost } from '../../interfaces/IPost';
-import styles from './PostItem.module.scss';
+import React, { ReactNode, useState } from 'react';
+
 import Button from '../UI/Button/Button';
 import Paragraph from '../UI/Paragraph/Paragraph';
 import Modal from '../UI/Modal/Modal';
+
+import { IPost } from '../../interfaces/IPost';
 import { postApi } from '../../services/PostService';
 import { IComments } from '../../interfaces/IComment';
+
+import styles from './PostItem.module.scss';
+import cn from 'classnames';
+import Spinner from '../UI/spinner/Spinner';
 
 type PropsPost = {
 	post: IPost;
@@ -21,6 +26,7 @@ export interface ChangePost {
 
 const PostItem = ({ post, remove, update, number }: PropsPost) => {
 	const [changePost, setChangePost] = useState<ChangePost>({ title: '', body: '' });
+	const [commentVisible, setCommentVisible] = useState<boolean>(false);
 	const [isVisible, setIsVisible] = useState<boolean>(false);
 
 	const handleRemove = (event: React.MouseEvent) => {
@@ -35,20 +41,38 @@ const PostItem = ({ post, remove, update, number }: PropsPost) => {
 	const { data: comments, isLoading, error } = postApi.useFetchCommentForPostQuery({});
 
 	const checkComments = () => {
-		comments.forEach((comment: IComments) => {
-			if (post.id === comment.postId) {
-				console.log(comment.name);
-			}
-		});
+		setCommentVisible((commentVisible) => !commentVisible);
 	};
 
 	return (
-		<div className={styles.post__container}>
+		<div
+			className={cn(styles.post__container, {
+				[styles.post__container_comments]: commentVisible,
+			})}>
 			<div className={styles.post__title_block}>
-				<Paragraph text={number + 1 + ':'} size={'large'} />
-				<Paragraph text={post.title} />
+				<Paragraph text={number + 1 + ':'} />
+				<Paragraph
+					text={post.title.substring(0, 1).toUpperCase() + post.title.substring(1)}
+				/>
 			</div>
-			<Paragraph text={post.body} size={'small'} />
+			<div className={styles.post__body}>
+				<Paragraph text={post.body} size={'small'} />
+				{!commentVisible && isLoading && <Spinner />}
+				{commentVisible
+					? comments.map((comment: IComments): JSX.Element | undefined => {
+							if (post.id === comment.postId) {
+								console.log(comment.postId);
+								return (
+									<div className={styles.post__comments}>
+										<Paragraph text={comment.email} size={'small'} />
+										<Paragraph text={comment.name} size={'small'} />
+									</div>
+								);
+							}
+					  })
+					: null}
+			</div>
+
 			<div className={styles.post__footer}>
 				<Button title='Remove' onClick={handleRemove} />
 				<Button title='Change post' onClick={onVisible} />
